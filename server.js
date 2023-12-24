c2dea9139ba280a4a7724995257f65d2e63cb1a7
Ekel <ekelleal@gmail.com>
2023-12-24 17:43:11 -0300

    primer compromiso

---
+++ server.js
@@ -0,0 +1,43 @@
+
+import express, { static as expressStatic, json } from 'express';
+import { readFileSync, writeFileSync } from 'fs';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(json());
+
+
+
+app.get('/fechas', (req, res) => {
+    const fechas = obtenerFechas();
+    res.json(fechas);
+});
+
+app.post('/fechas', (req, res) => {
+    const fecha = req.body.fecha;
+
+    if (fecha) {
+        const fechas = obtenerFechas();
+        fechas.push({ fecha });
+        guardarFechas(fechas);
+        res.json({ success: true, message: 'Fecha guardada con éxito.' });
+    } else {
+        res.status(400).json({ success: false, message: 'Fecha no proporcionada.' });
+    }
+});
+
+function obtenerFechas() {
+    const contenido = readFileSync('fechas.json', 'utf-8');
+    return JSON.parse(contenido);
+}
+
+function guardarFechas(fechas) {
+    const jsonString = JSON.stringify(fechas, null, 2);
+    writeFileSync('fechas.json', jsonString, 'utf-8');
+}
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
